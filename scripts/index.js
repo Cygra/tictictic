db2ce44e5a29ee5e38f8041ab0f41e6ada923f57
Cygra <sjtuwbh@gmail.com>
2020-07-03 21:47:18 +0800

    listen visibilitychange

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,18 @@
-$(document).ready(function () {
-  var ANIM_DURATION = 1500;
+const ANIM_DURATION = 1500;
+let secondHandInterval, minuteHandInterval, hourHandInterval;
 
-  var $secondHand = $($(".second-hand")[0]);
-  var $minuteHand = $($(".minute-hand")[0]);
-  var $hourHand = $($(".hour-hand")[0]);
+const rotateTo = ($e) => (deg) => {
+  $e.css({ transform: "rotate(" + deg + "deg)" });
+};
 
+const animate = (finalPosition, $elem) => {
+  $({ deg: 0 }).animate(
+    { deg: finalPosition },
+    { duration: ANIM_DURATION, step: rotateTo($elem) }
+  );
+};
+
+var didMount = ($secondHand, $minuteHand, $hourHand) => {
   var current = new Date();
   var second = current.getSeconds();
   var minute = current.getMinutes();
@@ -15,36 +23,40 @@ $(document).ready(function () {
   var minuteHandInitPos = minute * 6 + secondOffset / 10;
   var hourHandInitPos = (hour % 12) * 30 + minute / 2 + secondOffset / 120;
 
-  var animate = function (finalPosition, $elem) {
-    $({ deg: 0 }).animate(
-      { deg: finalPosition },
-      {
-        duration: ANIM_DURATION,
-        step: function (now) {
-          $elem.css({ transform: "rotate(" + now + "deg)" });
-        },
-      }
-    );
-  };
-
   animate(secondHandInitPos, $secondHand);
   animate(minuteHandInitPos, $minuteHand);
   animate(hourHandInitPos, $hourHand);
 
-  setTimeout(function () {
-    setInterval(function () {
-      secondHandInitPos += 1.2;
-      $secondHand.css({ transform: "rotate(" + secondHandInitPos + "deg)" });
-    }, 200);
+  setTimeout(() => {
+    secondHandInterval = setInterval(
+      () => rotateTo($secondHand)((secondHandInitPos += 1.2)),
+      200
+    );
 
-    setInterval(function () {
-      minuteHandInitPos += 0.05;
-      $minuteHand.css({ transform: "rotate(" + minuteHandInitPos + "deg)" });
-    }, 500);
+    minuteHandInterval = setInterval(
+      () => rotateTo($minuteHand)((minuteHandInitPos += 0.05)),
+      500
+    );
 
-    setInterval(function () {
-      hourHandInitPos += 0.01;
-      $hourHand.css({ transform: "rotate(" + hourHandInitPos + "deg)" });
-    }, 1200);
+    hourHandInterval = setInterval(
+      () => rotateTo($hourHand)((hourHandInitPos += 0.01)),
+      1200
+    );
   }, ANIM_DURATION);
+};
+
+$(document).ready(() => {
+  var $secondHand = $($(".second-hand")[0]);
+  var $minuteHand = $($(".minute-hand")[0]);
+  var $hourHand = $($(".hour-hand")[0]);
+
+  didMount($secondHand, $minuteHand, $hourHand);
+
+  document.addEventListener("visibilitychange", () => {
+    if (document.visibilityState === "hidden") {
+      clearInterval(secondHandInterval);
+      clearInterval(minuteHandInterval);
+      clearInterval(hourHandInterval);
+    } else didMount($secondHand, $minuteHand, $hourHand);
+  });
 });
